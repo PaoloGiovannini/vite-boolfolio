893714949051428175381c4727a7786c39e00e13
Paolo Giovannini <giovannini13@gmail.com>
2023-05-30 14:43:34 +0200

    chiamata axios per recupero dati (Milestone 3 e 4)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
-<script >
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
+export default{
+  components:{
+    AppHeader,
+    AppMain
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppMain.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+    import axios from 'axios';
+    export default{
+        name: "AppMain",
+        data(){
+            return{
+                projects: [],
+                url: 'http://localhost:8000',
+            }
+        },
+        methods:{
+            getProjects(){
+                axios.get(`${this.url}/api/projects`)
+                .then(response =>{
+                    console.log(response);
+                    this.projects = response.data.results;
+                });
+
+            }
+        },
+        mounted(){
+            this.getProjects();
+        }
+    }
+</script>
