e69c29cdbaeacf12d6871c442e3cc7cc172459d7
Paolo Giovannini <giovannini13@gmail.com>
2023-05-30 15:32:18 +0200

    esercizio concluso

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,30 @@
 <template>
+    <div class="container">
+        <div class="row">
+            <div class="col-4" v-for="project in projects">
+                <ProjectCard 
+                :title="project.title"
+                :content="project.content"
+                :image="project.image"
+                :url="url"
+                :type="project.type?.name"
+                />
+            </div>
+
+        </div>
+    </div>
 
 </template>
 
 <script>
     import axios from 'axios';
+    import ProjectCard from './ProjectCard.vue';
+
     export default{
         name: "AppMain",
+        components:{
+            ProjectCard
+        },
         data(){
             return{
                 projects: [],
@@ -18,6 +37,7 @@
                 .then(response =>{
                     console.log(response);
                     this.projects = response.data.results;
+                    console.log(this.projects)
                 });
 
             }
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <img v-if="image" :src="`${this.url}/storage/${image}`" class="card-img-top">
+        <h3 v-else class="text-center">Nessuna foto</h3>
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">{{ content }}</p>
+            <h5>{{type}}</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProjectCard",
+    props: {
+        title: String,
+        content:String,
+        image:String,
+        url:String,
+        type:String
+    }
+}
+</script>
