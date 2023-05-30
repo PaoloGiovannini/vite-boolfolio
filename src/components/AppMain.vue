d68af56431f660d8ae66e22e6f81a0358b029b2f
Paolo Giovannini <giovannini13@gmail.com>
2023-05-30 15:46:27 +0200

    esercizio bonus (paginazione) concluso

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-4" v-for="project in projects">
+            <div class="col-4 py-2" v-for="project in projects">
                 <ProjectCard 
                 :title="project.title"
                 :content="project.content"
@@ -12,6 +12,10 @@
             </div>
 
         </div>
+        <ul class="pagination">
+            <li class="page-item"><button class="page-link" @click="getProjects(currentPage - 1)" :class="{'disabled' : currentPage == 1}">Previous</button></li>
+            <li class="page-item"><button class="page-link" @click="getProjects(currentPage + 1)" :class="{'disabled' : currentPage == lastPage}">Next</button></li>
+        </ul>
     </div>
 
 </template>
@@ -29,21 +33,30 @@
             return{
                 projects: [],
                 url: 'http://localhost:8000',
+                currentPage: 1,
+                lastPage:null
             }
         },
         methods:{
-            getProjects(){
-                axios.get(`${this.url}/api/projects`)
+            getProjects(changePage){
+                axios.get(`${this.url}/api/projects`,
+                {
+                    params:{
+                        page:changePage
+                    }
+                }
+                )
                 .then(response =>{
                     console.log(response);
-                    this.projects = response.data.results;
-                    console.log(this.projects)
+                    this.projects = response.data.results.data;
+                    this.currentPage = response.data.results.current_page;
+                    this.lastPage = response.data.results.last_page;
                 });
 
             }
         },
         mounted(){
-            this.getProjects();
+            this.getProjects(1);
         }
     }
 </script>
