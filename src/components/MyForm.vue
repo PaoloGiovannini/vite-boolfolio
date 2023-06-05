9360d2515d07bc7c35da3423012d7bad841db39f
Paolo Giovannini <giovannini13@gmail.com>
2023-06-05 21:19:00 +0200

    esercizio concluso

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -1,19 +1,35 @@
 <template>
     <div class="container">
+
+        <div v-if="send" class="alert alert-primary" role="alert">
+            Messaggio inviato con successo!
+        </div>
         <form @submit.prevent="sendMyForm()">
             <div class="mb-3">
-                <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="email" v-model="email">
+                <label for="email" class="form-label">La tua email</label>
+                <input type="email" class="form-control" :class="{ 'is-invalid': errors.email }" id="email" v-model="email">
+                <div class="invalid-feedback" v-for="error in errors.email">
+                    {{ error }}
+                </div>
             </div>
             <div class="mb-3">
-                <label for="name" class="form-label">Nome:</label>
-                <input type="text" class="form-control" id="name" v-model="name">
+                <label for="name" class="form-label">Il tuo nome</label>
+                <input type="text" class="form-control" :class="{ 'is-invalid': errors.name }" id="name" v-model="name">
+                <div class="invalid-feedback" v-for="error in errors.name">
+                    {{ error }}
+                </div>
             </div>
             <div class="mb-3">
-                <label for="message">Messaggio:</label>
-                <textarea class="form-control" id="message" v-model="message"></textarea>
+                <label for="message">Il tuo messaggio</label>
+                <textarea class="form-control" :class="{ 'is-invalid': errors.message }" id="message"
+                    v-model="message"></textarea>
+                <div class="invalid-feedback" v-for="error in errors.message">
+                    {{ error }}
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary">Invio</button>
+            <button type="submit" class="btn btn-primary" :disabled="pending">
+                {{ pending ? 'Invio in corso...' : 'Invia messaggio' }}
+            </button>
         </form>
     </div>
 </template>
@@ -29,21 +45,38 @@ export default {
             name: '',
             email: '',
             message: '',
+            send: false,
+            pending: false,
+            errors: {}
         }
     },
-    methods:{
-        sendMyForm(){
+    methods: {
+        sendMyForm() {
 
-            
+            this.send = false
+            this.pending = true;
+            this.errors = {};
+
             axios.post(`${this.store.url}/api/contacts`,
-            {
-                name: this.name,
-                email: this.email,
-                message: this.message,
-            }
-            ).then(response =>{
-                console.log(response);
-            })
+                {
+                    name: this.name,
+                    email: this.email,
+                    message: this.message,
+                }
+            ).then(response => {
+                if (response.data.success) {
+                    this.name = '';
+                    this.email = '';
+                    this.message = '';
+                    this.send = true;
+                } else {
+                    this.errors = response.data.errors;
+                }
+
+                this.pending = false;
+            }).catch(error => {
+                this.pending = false;
+            });
         }
     }
 }
